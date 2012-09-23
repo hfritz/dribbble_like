ca2c57fb0ad7d772f8613611668c3809dfbfa38c
Helmut Fritz <helmut.fritz.v@gmail.com>
2012-09-23 16:55:59 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DribbbleTest::Application.config.secret_token = '78623e58e1e74572ffa1eb4d5907d983d04f4db0ebf80435ca014698eefb09f50faf515c7f9b782b206e7d8b51030766617ed9d71d983149272f5a9f585bc46d'
